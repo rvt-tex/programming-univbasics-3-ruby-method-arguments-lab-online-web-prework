82e1d6cd9b4174ceb6c30a7c09303931b10efa21
Ruel Tingling <rvtingling@yahoo.com>
2019-08-29 13:40:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,11 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
+
+ def introduction(name)
+ puts "Hi, my name is #{name}."
+end
+
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and i am learning to program in #{language}."
 end
@@ -12,7 +17,7 @@ def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and i am learning to program in #{language}."
 end
 
-introduction_with_language_optional("ruel")
+
 
 
 
