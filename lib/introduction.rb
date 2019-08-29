ed91a3497fbda9744e713b6925bdbc4c4135923b
Ruel Tingling <rvtingling@yahoo.com>
2019-08-29 13:37:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,10 +6,13 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and i am learning to program in #{language}."
 end
 
+introduction_with_language("ruel", "Swift")
+
 def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and i am learning to program in #{language}."
 end
 
+introduction_with_language_optional("ruel")
 
 
 
